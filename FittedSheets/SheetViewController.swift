b949789b8a396b6ea9eced79c7c36185d67b5a10
Artur <artur.r@flypika.com>
2022-10-29 16:06:51 +0300

    [*] Add shouldDismiss when pull to dismiss

--- FittedSheets/SheetViewController.swift
+++ FittedSheets/SheetViewController.swift
@@ -417,20 +417,22 @@ open class SheetViewController: UIViewController {
                 
                 guard finalHeight > 0 || !self.dismissOnPull else {
                     // Dismiss
-                    UIView.animate(
-                        withDuration: animationDuration,
-                        delay: 0,
-                        usingSpringWithDamping: self.options.transitionDampening,
-                        initialSpringVelocity: self.options.transitionVelocity,
-                        options: self.options.transitionAnimationOptions,
-                        animations: {
-                        self.contentViewController.view.transform = CGAffineTransform(translationX: 0, y: self.contentViewController.view.bounds.height)
-                        self.view.backgroundColor = UIColor.clear
-                        self.transition.setPresentor(percentComplete: 1)
-                        self.overlayView.alpha = 0
-                    }, completion: { complete in
-                        self.attemptDismiss(animated: false)
-                    })
+                    if self.shouldDismiss?(self) != false {
+                        UIView.animate(
+                            withDuration: animationDuration,
+                            delay: 0,
+                            usingSpringWithDamping: self.options.transitionDampening,
+                            initialSpringVelocity: self.options.transitionVelocity,
+                            options: self.options.transitionAnimationOptions,
+                            animations: {
+                                self.contentViewController.view.transform = CGAffineTransform(translationX: 0, y: self.contentViewController.view.bounds.height)
+                                self.view.backgroundColor = UIColor.clear
+                                self.transition.setPresentor(percentComplete: 1)
+                                self.overlayView.alpha = 0
+                            }, completion: { complete in
+                                self.attemptDismiss(animated: false)
+                            })
+                    }
                     return
                 }
                 
